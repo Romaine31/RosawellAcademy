4235f73edc1566cf92e1878313cc2d0653e61a77
Romaine <rcagubcub2@gmail.com>
2023-12-04 23:17:38 +0800

    npc's adjustments

---
+++ Assets/Scripts/scriptableObjects/stringValue.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class stringValue : ScriptableObject
+{
+    [SerializeField]
+    public List<string> stringVal;
+}
