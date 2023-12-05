54b3215be226e73ec3e266fef64e9ddb58248e8e
3isley <eisley22maglabe@gmail.com>
2023-12-06 06:07:44 +0800

    ANIMATION AND QUESTION

--- Assets/NPCs/aiTest.cs
+++ Assets/NPCs/aiTest.cs
@@ -6,6 +6,9 @@ using UnityEngine.AI;
 public class aiTest : MonoBehaviour
 {
      private NavMeshAgent agent;
+     public Animator animator;
+
+     Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,14 @@ public class aiTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal",movement.x);
+        animator.SetFloat("Vertical",movement.y);
+        animator.SetFloat("Speed",movement.magnitude);
+
         if (Input.GetMouseButtonUp(0))
         {
             var target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
